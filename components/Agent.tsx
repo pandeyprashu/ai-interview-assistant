58417892e23f1941bd6764efbacb285e2b9e6b73
Prashu Pandey <prashupandey0210@gmail.com>
2025-04-15 22:36:55 +0530

    Fixed Bug

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
+import { interviewer } from "@/constants";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -24,11 +25,117 @@ const Agent=({
     type,
     questions,
   }: AgentProps)=> {
-  const router = useRouter();
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
   const [messages, setMessages] = useState<SavedMessage[]>([]);
   const [isSpeaking, setIsSpeaking] = useState(false);
   const [lastMessage, setLastMessage] = useState<string>("");
+
+  useEffect(()=>{
+    const onCallStart=()=>{
+      setCallStatus(CallStatus.ACTIVE);
+    }
+    const onCallEnd=()=>{
+      setCallStatus(CallStatus.FINISHED);
+    }
+
+    const onMessage=(message:Message)=>{
+      if(message.type==='transcript'&&message.transcriptType==='final'){
+        const newMessage={role:message.role,content:message.transcript};
+        setMessages((prevMessages) => [...prevMessages, newMessage]);
+    }
+
+  }
+  const onSpeechStart = () => {
+    console.log("speech start");
+    setIsSpeaking(true);
+  };
+
+  const onSpeechEnd = () => {
+    console.log("speech end");
+    setIsSpeaking(false);
+  };
+
+  const onError = (error: Error) => {
+    console.log("Error:", error);
+  };
+  vapi.on("call-start", onCallStart);
+  vapi.on("call-end", onCallEnd);
+  vapi.on("message", onMessage);
+  vapi.on("speech-start", onSpeechStart);
+  vapi.on("speech-end", onSpeechEnd);
+  vapi.on("error", onError);
+
+  return () => {
+    vapi.off("call-start", onCallStart);
+    vapi.off("call-end", onCallEnd);
+    vapi.off("message", onMessage);
+    vapi.off("speech-start", onSpeechStart);
+    vapi.off("speech-end", onSpeechEnd);
+    vapi.off("error", onError);
+  };
+
+  },[]);
+  if (messages.length > 0) {
+    setLastMessage(messages[messages.length - 1].content);
+  }
+  console.log(interviewId);
+  console.log(feedbackId);
+
+//   useEffect(()=>{   
+
+//   // const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+//   //   console.log("handleGenerateFeedback");
+
+//   //   const { success, feedbackId: id } = await createFeedback({
+//   //     interviewId: interviewId!,
+//   //     userId: userId!,
+//   //     transcript: messages,
+//   //     feedbackId,
+//   //   });
+
+//   //   if (success && id) {
+//   //     router.push(`/interview/${interviewId}/feedback`);
+//   //   } else {
+//   //     console.log("Error saving feedback");
+//   //     router.push("/");
+//   //   }
+//   // };
+
+//   if (callStatus === CallStatus.FINISHED) {
+//     if (type === "generate") {
+//       router.push("/");
+//     } else {
+//       handleGenerateFeedback(messages);
+//     }
+//   }
+// }, [messages, callStatus, feedbackId, interviewId, router, type, userId]);
+
+  const handleCall=async()=>{
+    setCallStatus(CallStatus.CONNECTING);
+    if(type=="generate")
+    {
+      await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,{
+        variableValues:{
+          username:userName,
+          userId:userId,
+        }
+      });
+    }else{
+      let formattedQuestions="";
+      if(questions){
+        formattedQuestions=questions.map((question)=>`-${question}`).join("\n");
+      }
+      await vapi.start(interviewer,{
+        variableValues:{
+          questions:formattedQuestions,
+        }
+      })
+    }
+  }
+  const handleDisconnect=()=>{
+    setCallStatus(CallStatus.FINISHED);
+    vapi.stop();
+  }
   return (
     <>
       <div className="call-view">
